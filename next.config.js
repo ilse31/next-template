f3feb707e314c956c194dcc095a20a7b58217347
ilse31 <ilhamprazsetyo33@gmail.com>
2023-04-08 22:45:42 +0700

    add some config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,29 @@
 /** @type {import('next').NextConfig} */
+
+const BASE_API_URL = process.env.ENABLE_MOCK_API
+  ? "https://localhost:3000/api"
+  : process.env.BASE_API_URL || "https://localhost:3000/api";
+
 const nextConfig = {
+  reactStrictMode: process.env.NODE_ENV === "production",
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+  rewrites: async () => [
+    {
+      source: "/api/:path*",
+      destination: BASE_API_URL + "/:path*",
+    },
+  ],
   experimental: {
     appDir: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
